8065ce77db6b8bf785b149dfd923115025769dfc
Muhammad Fayaad <fayet256@gmail.com>
2022-01-10 02:34:20 +0700

    push

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>EHEHEHEHHEHE</p>
+        <p>EHEHEHEHHEHE INI BARU BISA</p>
         <a
           className="App-link"
           href="https://reactjs.org"
